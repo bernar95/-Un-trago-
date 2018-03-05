01add295faf8ce58a8d971ea7a5e979658a10614
Bernardo <berny.sevillista@gmail.com>
2018-03-05 16:37:14 +0100

    Sistema de progresión de días
    Realizado el sistema de paso de días.

---
+++ Scripts/Tiempo.gd
@@ -0,0 +1,70 @@
+extends Node
+
+var diasAbierto = 1
+
+var tiempo = Timer.new()
+
+var labelDia
+var labelMomento
+var labelTiempo
+var aparecer = false
+
+func _ready():
+	tiempo.set_one_shot(true)
+	self.add_child(tiempo)
+	set_fixed_process(true)
+	labelDia = get_node("Hud/Dia")
+	labelMomento = get_node("Hud/MomentoDia")
+	labelTiempo = get_node("Hud/Tiempo")
+	tiempo()
+	pass
+
+func _fixed_process(delta):
+	labelTiempo.set_text(str(int(tiempo.get_time_left())))
+	 
+	if aparecer == true:
+		labelTiempo.show()
+	else:
+		labelTiempo.hide()
+	
+
+func tiempo():
+	labelDia.set_text("Día" + " " + str(diasAbierto))
+	labelDia.show()
+	tiempo.set_wait_time(5)
+	tiempo.start()
+	yield(tiempo, "timeout")
+	labelDia.hide()
+	
+	labelMomento.set_text("Hora de hacer pedidos")
+	aparecer = true
+	tiempo.set_wait_time(11)
+	tiempo.start()
+	yield(tiempo, "timeout")
+	
+	labelMomento.set_text("Mañana")
+	tiempo.set_wait_time(15)
+	tiempo.start()
+	yield(tiempo, "timeout")
+	
+	labelMomento.set_text("Tarde")
+	tiempo.set_wait_time(12)
+	tiempo.start()
+	yield(tiempo, "timeout")
+	
+	labelMomento.set_text("Noche")
+	tiempo.set_wait_time(14)
+	tiempo.start()
+	yield(tiempo, "timeout")
+	
+	diasAbierto += 1
+	get_node("Hud/Dia").set_text("")
+	labelMomento.set_text("")
+	aparecer = false
+	tiempo()
+	
+	
+	
+	
+	
+	
