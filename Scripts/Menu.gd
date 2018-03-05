c5fdd89979f08b875047d4b8b69bf3b75c687196
Bernardo <berny.sevillista@gmail.com>
2018-03-05 16:38:25 +0100

    Menú principal
    Primera versión del menú principal, con el botón de nueva partida
    funcional.

---
+++ Scripts/Menu.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _ready():
+	
+	var nuevaPartida = get_node("NuevaPartida")
+	
+	if nuevaPartida:
+		nuevaPartida.connect("pressed", self, "_nueva_partida")
+	pass
+
+func _nueva_partida():
+	get_tree().change_scene("res://Scenes/Taberna.tscn")
+	
+	
