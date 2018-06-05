03e9c5609d777e99639b235aecadd331ae3dadb0
Bernardo <berny.sevillista@gmail.com>
2018-06-05 15:13:50 +0200

    Cambios en script de cajas
    Se han llevado cambios en el script de cajas de comida para acortar el
    código.

--- Scripts/Caja.gd
+++ Scripts/Caja.gd
@@ -3,7 +3,16 @@ extends Node
 var personaje
 var notificaciones
 
+var carne = false
+var pescado = false
+var verduras = false
+var patatas = false
+var huevos = false
+var pan = false
+var queso = false
+
 func _ready():
+	set_fixed_process(true)
 	notificaciones = get_parent().get_parent().get_node("Hud/Notificaciones")
 	personaje = get_parent().get_parent().get_node("KinematicBody2D/Personaje")
 	get_node("CajaCarne").hide()
@@ -14,6 +23,43 @@ func _ready():
 	get_node("CajaQueso").hide()
 	get_node("CajaHuevos").hide()
 	pass
+
+func _fixed_process(delta):
+	if get_parent().get_parent().get_node("Hud/Container").stock_carne > 0 and carne == false:
+		global.carne3 = global.spriteCarne.instance()
+		global.carne3.set_pos(Vector2(290, 42))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Carne").add_child(global.carne3)
+		carne = true
+	elif get_parent().get_parent().get_node("Hud/Container").stock_pescado > 0 and pescado == false:
+		global.pescado3 = global.spritePescado.instance()
+		global.pescado3.set_pos(Vector2(640, 42))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Pescado").add_child(global.pescado3)
+		pescado = true
+	elif get_parent().get_parent().get_node("Hud/Container").stock_verduras > 0 and verduras == false:
+		global.verdura3 = global.spriteVerduras.instance()
+		global.verdura3.set_pos(Vector2(704, 42))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Verduras").add_child(global.verdura3)
+		verduras = true
+	elif get_parent().get_parent().get_node("Hud/Container").stock_patatas > 0 and patatas == false:
+		global.patata3 = global.spritePatatas.instance()
+		global.patata3.set_pos(Vector2(736, 42))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Patatas").add_child(global.patata3)
+		patatas = true
+	elif get_parent().get_parent().get_node("Hud/Container").stock_pan > 0 and pan == false:
+		global.pan3 = global.spritePan.instance()
+		global.pan3.set_pos(Vector2(512, 26))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Pan").add_child(global.pan3)
+		pan = true
+	elif get_parent().get_parent().get_node("Hud/Container").stock_queso > 0 and queso == false:
+		global.queso3 = global.spriteQueso.instance()
+		global.queso3.set_pos(Vector2(672, 42))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Queso").add_child(global.queso3)
+		queso = true
+	elif get_parent().get_parent().get_node("Hud/Container").stock_huevos > 0 and huevos == false:
+		global.huevo3 = global.spriteHuevos.instance()
+		global.huevo3.set_pos(Vector2(544, 26))
+		get_parent().get_parent().get_node("BarraDetras/Alimentos/Huevos").add_child(global.huevo3)
+		huevos = true
 #Esta parte gestiona la interracción con las cajas de alimentos, y lo que
 #hace es que le aparezcan en las manos del personaje los alimentos, 
 #teniendo en cuenta si las manos están ocupadas o no y si tienes stock
@@ -39,30 +85,37 @@ func interaccion_caja(result):
 			cogerComida(global.carne)
 			if get_parent().get_parent().get_node("Hud/Container").stock_carne == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Carne"))
+				carne = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_pescado > 0 and result[0].collider.has_node("CajaPescadoInteraccion"):
 			cogerComida(global.pescado)
 			if get_parent().get_parent().get_node("Hud/Container").stock_pescado == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Pescado"))
+				pescado = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_verduras > 0 and result[0].collider.has_node("CajaVerdurasInteraccion"):
 			cogerComida(global.verdura)
 			if get_parent().get_parent().get_node("Hud/Container").stock_verduras == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Verduras"))
+				verduras = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_patatas > 0 and result[0].collider.has_node("CajaPatatasInteraccion"):
 			cogerComida(global.patata)
 			if get_parent().get_parent().get_node("Hud/Container").stock_patatas == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Patatas"))
+				patatas = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_pan > 0 and result[0].collider.has_node("CajaPanInteraccion"):
 			cogerComida(global.pan)
 			if get_parent().get_parent().get_node("Hud/Container").stock_pan == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Pan"))
+				pan = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_queso > 0 and result[0].collider.has_node("CajaQuesoInteraccion"):
 			cogerComida(global.queso)
 			if get_parent().get_parent().get_node("Hud/Container").stock_queso == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Queso"))
+				queso = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_huevos > 0 and result[0].collider.has_node("CajaHuevosInteraccion"):
 			cogerComida(global.huevo)
 			if get_parent().get_parent().get_node("Hud/Container").stock_huevos == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Huevos"))
+				huevos = false
 	elif personaje.get_child_count() == 1:
 		if get_parent().get_parent().get_node("Hud/Container").stock_carne > 0 and result[0].collider.has_node("CajaCarneInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
@@ -71,6 +124,7 @@ func interaccion_caja(result):
 				cogerComida(global.carne)
 			if get_parent().get_parent().get_node("Hud/Container").stock_carne == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Carne"))
+				carne = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_pescado > 0 and result[0].collider.has_node("CajaPescadoInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
 				cogerComida(global.pescado2)
@@ -78,6 +132,7 @@ func interaccion_caja(result):
 				cogerComida(global.pescado)
 			if get_parent().get_parent().get_node("Hud/Container").stock_pescado == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Pescado"))
+				pescado = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_verduras > 0 and result[0].collider.has_node("CajaVerdurasInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
 				cogerComida(global.verdura2)
@@ -85,6 +140,7 @@ func interaccion_caja(result):
 				cogerComida(global.verdura)
 			if get_parent().get_parent().get_node("Hud/Container").stock_verduras == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Verduras"))
+				verduras = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_patatas > 0 and result[0].collider.has_node("CajaPatatasInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
 				cogerComida(global.patata2)
@@ -92,6 +148,7 @@ func interaccion_caja(result):
 				cogerComida(global.patata)
 			if get_parent().get_parent().get_node("Hud/Container").stock_patatas == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Patatas"))
+				patatas = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_pan > 0 and result[0].collider.has_node("CajaPanInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
 				cogerComida(global.pan2)
@@ -99,6 +156,7 @@ func interaccion_caja(result):
 				cogerComida(global.pan)
 			if get_parent().get_parent().get_node("Hud/Container").stock_pan == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Pan"))
+				pan = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_queso > 0 and result[0].collider.has_node("CajaQuesoInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
 				cogerComida(global.queso2)
@@ -106,6 +164,7 @@ func interaccion_caja(result):
 				cogerComida(global.queso)
 			if get_parent().get_parent().get_node("Hud/Container").stock_queso == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Queso"))
+				queso = false
 		elif get_parent().get_parent().get_node("Hud/Container").stock_huevos > 0 and result[0].collider.has_node("CajaHuevosInteraccion"):
 			if personaje.get_child(0).get_pos() == global.mano_derecha:
 				cogerComida(global.huevo2)
@@ -113,6 +172,7 @@ func interaccion_caja(result):
 				cogerComida(global.huevo)
 			if get_parent().get_parent().get_node("Hud/Container").stock_huevos == 0:
 				eliminar_sprites(get_parent().get_parent().get_node("BarraDetras/Alimentos/Huevos"))
+				huevos = false
 	elif personaje.get_child_count() == 2:
 		notificaciones.notificaciones("Tienes las manos ocupadas")
 
@@ -126,8 +186,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Carne", "0")
 			global.instanciar_ingredientes(global.carne2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Carne").get_child_count() == 0:
-			aparecer_sprites(global.carne3)
 	elif (personaje.get_child_count() == 1 or personaje.get_child_count() == 2) and result[0].collider.has_node("CajaPescadoInteraccion") and personaje.get_child(0).get_filename() == global.spritePescado.get_path():
 		if personaje.get_child(0).get_pos() == global.mano_derecha:
 			dejarComida("Pescado", "0")
@@ -135,8 +193,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Pescado", "0")
 			global.instanciar_ingredientes(global.pescado2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Pescado").get_child_count() == 0:
-			aparecer_sprites(global.pescado3)
 	elif (personaje.get_child_count() == 1 or personaje.get_child_count() == 2) and result[0].collider.has_node("CajaVerdurasInteraccion") and personaje.get_child(0).get_filename() == global.spriteVerduras.get_path():
 		if personaje.get_child(0).get_pos() == global.mano_derecha:
 			dejarComida("Verdura", "0")
@@ -144,8 +200,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Verdura", "0")
 			global.instanciar_ingredientes(global.verdura2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Verduras").get_child_count() == 0:
-			aparecer_sprites(global.verdura3)
 	elif (personaje.get_child_count() == 1 or personaje.get_child_count() == 2) and result[0].collider.has_node("CajaPatatasInteraccion") and personaje.get_child(0).get_filename() == global.spritePatatas.get_path():
 		if personaje.get_child(0).get_pos() == global.mano_derecha:
 			dejarComida("Patata", "0")
@@ -153,8 +207,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Patata", "0")
 			global.instanciar_ingredientes(global.patata2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Patatas").get_child_count() == 0:
-			aparecer_sprites(global.patata3)
 	elif (personaje.get_child_count() == 1 or personaje.get_child_count() == 2) and result[0].collider.has_node("CajaPanInteraccion") and personaje.get_child(0).get_filename() == global.spritePan.get_path():
 		if personaje.get_child(0).get_pos() == global.mano_derecha:
 			dejarComida("Pan", "0")
@@ -162,8 +214,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Pan", "0")
 			global.instanciar_ingredientes(global.pan2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Pan").get_child_count() == 0:
-			aparecer_sprites(global.pan3)
 	elif (personaje.get_child_count() == 1 or personaje.get_child_count() == 2) and result[0].collider.has_node("CajaQuesoInteraccion") and personaje.get_child(0).get_filename() == global.spriteQueso.get_path():
 		if personaje.get_child(0).get_pos() == global.mano_derecha:
 			dejarComida("Queso", "0")
@@ -171,8 +221,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Queso", "0")
 			global.instanciar_ingredientes(global.queso2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Queso").get_child_count() == 0:
-			aparecer_sprites(global.queso3)
 	elif (personaje.get_child_count() == 1 or personaje.get_child_count() == 2) and result[0].collider.has_node("CajaHuevosInteraccion") and personaje.get_child(0).get_filename() == global.spriteHuevos.get_path():
 		if personaje.get_child(0).get_pos() == global.mano_derecha:
 			dejarComida("Huevo", "0")
@@ -180,8 +228,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Huevo", "0")
 			global.instanciar_ingredientes(global.huevo2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Huevos").get_child_count() == 0:
-			aparecer_sprites(global.huevo3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaCarneInteraccion") and personaje.get_child(1).get_filename() == global.spriteCarne.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Carne", "1")
@@ -189,8 +235,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Carne", "1")
 			global.instanciar_ingredientes(global.carne2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Carne").get_child_count() == 0:
-			aparecer_sprites(global.carne3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaPescadoInteraccion") and personaje.get_child(1).get_filename() == global.spritePescado.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Pescado", "1")
@@ -198,8 +242,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Pescado", "1")
 			global.instanciar_ingredientes(global.pescado2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Pescado").get_child_count() == 0:
-			aparecer_sprites(global.pescado3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaVerdurasInteraccion") and personaje.get_child(1).get_filename() == global.spriteVerduras.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Verdura", "1")
@@ -207,8 +249,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Verdura", "1")
 			global.instanciar_ingredientes(global.verdura2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Verduras").get_child_count() == 0:
-			aparecer_sprites(global.verdura3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaPatatasInteraccion") and personaje.get_child(1).get_filename() == global.spritePatatas.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Patata", "1")
@@ -216,8 +256,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Patata", "1")
 			global.instanciar_ingredientes(global.patata2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Patatas").get_child_count() == 0:
-			aparecer_sprites(global.patata3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaPanInteraccion") and personaje.get_child(1).get_filename() == global.spritePan.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Pan", "1")
@@ -225,8 +263,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Pan", "1")
 			global.instanciar_ingredientes(global.pan2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Pan").get_child_count() == 0:
-			aparecer_sprites(global.pan3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaQuesoInteraccion") and personaje.get_child(1).get_filename() == global.spriteQueso.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Queso", "1")
@@ -234,8 +270,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Queso", "1")
 			global.instanciar_ingredientes(global.queso2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Queso").get_child_count() == 0:
-			aparecer_sprites(global.queso3)
 	elif personaje.get_child_count() == 2 and result[0].collider.has_node("CajaHuevosInteraccion") and personaje.get_child(1).get_filename() == global.spriteHuevos.get_path():
 		if personaje.get_child(1).get_pos() == global.mano_derecha:
 			dejarComida("Huevo", "1")
@@ -243,8 +277,6 @@ func dejar_alimento(result):
 		else:
 			dejarComida("Huevo", "1")
 			global.instanciar_ingredientes(global.huevo2)
-		if get_parent().get_parent().get_node("BarraDetras/Alimentos/Huevos").get_child_count() == 0:
-			aparecer_sprites(global.huevo3)
 	else:
 		notificaciones.notificaciones("Esto no va aquí")
 
@@ -283,37 +315,6 @@ func cogerComida(comida):
 func eliminar_sprites(ruta):
 	for i in range(0, ruta.get_child_count()):
 		ruta.get_child(i).queue_free()
-
-func aparecer_sprites(ingrediente):
-	if ingrediente == global.carne3:
-		global.carne3 = global.spriteCarne.instance()
-		global.carne3.set_pos(Vector2(290, 42))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Carne").add_child(global.carne3)
-	elif ingrediente == global.pescado3:
-		global.pescado3 = global.spritePescado.instance()
-		global.pescado3.set_pos(Vector2(640, 42))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Pescado").add_child(global.pescado3)
-	elif ingrediente == global.verdura3:
-		global.verdura3 = global.spriteVerduras.instance()
-		global.verdura3.set_pos(Vector2(704, 42))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Verduras").add_child(global.verdura3)
-	elif ingrediente == global.patata3:
-		global.patata3 = global.spritePatatas.instance()
-		global.patata3.set_pos(Vector2(736, 42))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Patatas").add_child(global.patata3)
-	elif ingrediente == global.pan3:
-		global.pan3 = global.spritePan.instance()
-		global.pan3.set_pos(Vector2(512, 26))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Pan").add_child(global.pan3)
-	elif ingrediente == global.queso3:
-		global.queso3 = global.spriteQueso.instance()
-		global.queso3.set_pos(Vector2(672, 42))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Queso").add_child(global.queso3)
-	elif ingrediente == global.huevo3:
-		global.huevo3 = global.spriteHuevos.instance()
-		global.huevo3.set_pos(Vector2(544, 26))
-		get_parent().get_parent().get_node("BarraDetras/Alimentos/Huevos").add_child(global.huevo3)
-
 #Esta función sirve para volver a dejar un alimento en una caja o para
 #dejar un plato de comida en el caldero
 func dejarComida(comida, hijo):
