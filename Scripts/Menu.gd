e317e9604212c988f42b16b438ee3f5985bd6ac8
Bernardo <berny.sevillista@gmail.com>
2018-06-07 16:53:03 +0200

    Salir del juego
    Implementada la opción de salir del juego.

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -5,9 +5,15 @@ extends Node
 func _ready():
 	
 	var nuevaPartida = get_node("NuevaPartida")
+	var salir = get_node("Salir")
 	
 	if nuevaPartida:
 		nuevaPartida.connect("pressed", self, "_nueva_partida")
+	if salir:
+		salir.connect("pressed", self, "_salir_juego")
 
 func _nueva_partida():
 	get_tree().change_scene("res://Scenes/Taberna.tscn")
+
+func _salir_juego():
+	get_tree().quit()
